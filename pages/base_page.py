60bfba91a388472a39e03b050775fbbf083de7e7
nastya-irbis <nastya.kant@gmail.com>
2019-09-11 19:59:06 +0300

    Добавлены методы  is_not_element_present и is_disappeared

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,8 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from .locators import BasePageLocators
 import math
 
@@ -19,6 +22,23 @@ class BasePage():
 			return False
 		return True
 	
+	def is_not_element_present(self, how, what, timeout=4):
+		try:
+			WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+		except TimeoutException:
+			return True
+			
+		return False
+	
+	def is_disappeared(self, how, what, timeout=4):
+		try:
+			WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+				until_not(EC.presence_of_element_located((how, what)))
+		except TimeoutException:
+			return False
+		
+		return True
+			
 	def go_to_login_page(self):
 		link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
 		link.click()
